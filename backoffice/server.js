6ec0507915eb5ad6d1c766e1b47b730d963161c0
Github_Name <petemount@t-online.de>
2024-05-17 17:00:57 +0200

    HA170524ExpressWebApp

---
+++ backoffice/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+// GETs für die HTML-Sites
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/home.html");
+});
+app.get("/kontakt", (req, res) => {
+    res.sendFile(__dirname + "/kontakt.html");
+});
+app.get("/aboutUs", (req, res) => {
+    res.sendFile(__dirname + "/aboutUs.html");
+});
+app.get("/impressum", (req, res) => {
+    res.sendFile(__dirname + "/impressum.html");
+});
+app.get("/datenschutz", (req, res) => {
+    res.sendFile(__dirname + "/datenschutz.html");
+});
+
+
+
+// Server aufrufen
+
+app.listen(PORT, () => {
+    console.log(`Der Server wurde gestartet auf Port: ${PORT}`);
+  });
